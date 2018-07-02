79bb3411536c9d45a554e8ebfa831aaffcb2d9f7
Wyatt Ades <wyattades@gmail.com>
2018-07-01 18:40:14 -0700

    Added about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,43 @@
 import React from 'react';
 
-import { ContainerSection } from './misc';
+import { SmallSection } from './misc';
+import services from '../services';
 
 export default () => (
-  <ContainerSection>
+  <SmallSection>
     <h1 className="is-size-1">About</h1>
-    <p>TBD</p>
-  </ContainerSection>
+    <br/>
+    <div className="content">
+      <h3>Find movies! Share songs! Discord servers! Clicking things!</h3>
+      <p>
+        If any of the above exclamations interest you, then you are in the
+        right place. Tely is a media aggregation platform that integrates with your
+        Discord friends and servers so you can share another aspect of your
+        personal life!
+      </p>
+      <h4>
+        Use features such as:
+      </h4>
+      <ul>
+        <li>Creating lists of media</li>
+        <li>Sharing lists of media</li>
+        <li>Deleting lists of media</li>
+      </ul>
+      <h4>With the following media types:</h4>
+      <ul>
+        {services.asArray.map(({ ID, LABEL, DESCRIPTION }) => <li key={ID}><strong>{LABEL}:</strong> {DESCRIPTION}</li>)}
+      </ul>
+      <br/>
+      <p>
+        <i>This application is currently in development</i>, so many features
+        are on the way. If you don't care, log in to get started!
+      </p>
+      <br/>
+      <pre>
+        Tely is an <a href="https://github.com/wyattades/tely">Open Source</a> app created by <a href="https://wyattades.com">Wyatt Ades</a>
+        <br/>
+        Copyright © 2018
+      </pre>
+    </div>
+  </SmallSection>
 );
