7e56f4d7e59d50f5808c900bba3ab11288422b42
Wyatt Ades <wyattades@gmail.com>
2018-07-16 18:50:56 -0700

    Add url query params for Browse

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { addUrlProps, UrlQueryParamTypes } from 'react-url-query';
 
-import { ContainerSection } from './misc';
+import { ContainerSection, Spinner } from './misc';
 import * as db from '../db';
 import { ListView } from './MediaLists';
 import services from '../services';
@@ -11,7 +12,12 @@ const _ListView = (props) => (
   </div>
 );
 
-export default class Browse extends React.Component {
+const urlPropsQueryConfig = {
+  sort: {},
+  filter: {},
+};
+
+class Browse extends React.Component {
 
   state = {
     lists: null,
@@ -19,8 +25,30 @@ export default class Browse extends React.Component {
   }
 
   componentDidMount() {
-    // TODO: implement sort-by and filter-by-type
-    this.unsubscribe = db.lists.where('is_public', '==', true)
+    this.fetchLists(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.filter !== nextProps.filter || this.props.sort !== nextProps.sort)
+      this.fetchLists(nextProps);
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  fetchLists = ({ sort, filter }) => {
+    if (this.unsubscribe) this.unsubscribe();
+
+    let query = db.lists.where('is_public', '==', true);
+
+    if (filter) query = query.where('type', '==', filter);
+
+    if (sort === 'new') query = query.orderBy('created');
+    else if (sort === 'modified') query = query.orderBy('modified');
+    else query = query.orderBy('popularity', 'desc');
+
+    this.unsubscribe = query
     .onSnapshot((snap) => {
       const lists = [];
       snap.forEach((item) => {
@@ -32,18 +60,23 @@ export default class Browse extends React.Component {
     }, (error) => console.error(error) || this.setState({ error: error.code }));
   }
 
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
+  changeFilter = (e) => this.props.onChangeFilter(e.target.value);
+
+  changeSort = (e) => this.props.onChangeSort(e.target.value);
 
   render() {
     const { lists, error } = this.state;
+    const { sort, filter } = this.props;
 
     let amount = null;
-    if (lists) {
+    if (error) {
+      amount = <p className="has-text-danger">Error: {error}</p>;
+    } else if (lists) {
       if (lists.length === 0) amount = 'No Results Found';
       else if (lists.length === 1) amount = '1 Result Found';
       else amount = `${lists.length} Results Found`;
+      
+      amount = <p className="has-text-grey">{amount}</p>;
     }
 
     return (
@@ -51,11 +84,10 @@ export default class Browse extends React.Component {
         <h1 className="is-size-1">Browse</h1>
         <p className="is-size-5 has-text-grey">Public Lists</p>
         <br/>
-        { error && <p className="has-text-danger">{error}</p>}
         <div className="level">
           <div className="level-left">
             <div className="level-item">
-              <p className="has-text-grey">{amount}</p>
+              {amount}
             </div>
           </div>
           <div className="level-right">
@@ -66,10 +98,10 @@ export default class Browse extends React.Component {
                 </div>
                 <div className="control">
                   <div className="select">
-                    <select>
-                      <option>-</option>
+                    <select value={filter || ''} onChange={this.changeFilter}>
+                      <option value="">-</option>
                       {services.asArray.map(({ ID, LABEL }) => (
-                        <option key={ID}>{LABEL}</option>
+                        <option key={ID} value={ID}>{LABEL}</option>
                       ))}
                     </select>
                   </div>
@@ -83,10 +115,10 @@ export default class Browse extends React.Component {
                 </div>
                 <div className="control">
                   <div className="select">
-                    <select>
-                      <option>Newest</option>
-                      <option>Popularity</option>
-                      <option>Last Modified</option>
+                    <select value={sort || ''} onChange={this.changeSort}>
+                      <option value="">Popularity</option>
+                      <option value="new">Newest</option>
+                      <option value="modified">Last Modified</option>
                     </select>
                   </div>
                 </div>
@@ -94,10 +126,14 @@ export default class Browse extends React.Component {
             </div>
           </div>
         </div>
-        <div className="columns is-multiline">
-          { lists && lists.length ? lists.map(_ListView) : null }
-        </div>
+        { lists ? (
+          <div className="columns is-multiline">
+            {lists.map(_ListView)}
+          </div>
+        ) : <><br/><br/><br/><Spinner centered/></> }
       </ContainerSection>
     );
   }
 }
+
+export default addUrlProps({ urlPropsQueryConfig })(Browse);
