f54c1c2e9098930c83c1d571a734cedad76053f1
Wyatt Ades <wyattades@gmail.com>
2018-07-21 14:47:26 -0700

    Fixed popularity

--- functions/triggers.js
+++ functions/triggers.js
@@ -9,9 +9,9 @@ const lists = firestore.collection('lists');
 exports.listUpdate = functions.firestore
 .document('lists/{listId}/contents/{contentId}')
 .onWrite((change, ctx) => (
-  firestore.runTransaction((ref) => (
-    ref.get(lists.doc(ctx.params.listId))
-    .then((doc) => ref.update({
+  firestore.runTransaction((trans) => (
+    trans.get(lists.doc(ctx.params.listId))
+    .then((doc) => trans.update(doc.ref, {
       popularity: (doc.data().popularity || 0) + 1,
       modified: Date.now(),
     }))
