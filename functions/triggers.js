6704fae3d58433402caa0bb384783f5eacb7d300
Wyatt Ades <wyattades@gmail.com>
2018-11-28 00:10:37 -0800

    clean functions

--- functions/triggers.js
+++ functions/triggers.js
@@ -1,44 +1,9 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
+const { deleteCollection } = require('./utils');
 
 const firestore = admin.firestore();
 const lists = firestore.collection('lists');
-
-
-const deleteQueryBatch = (query, batchSize, resolve, reject) => {
-  query.get()
-  .then((snap) => {
-    // When there are no documents left, we are done
-    if (!snap.size)
-      return 0;
-
-    // Delete documents in a batch
-    const batch = firestore.batch();
-    for (const doc of snap.docs) batch.delete(doc.ref);
-
-    return batch.commit()
-    .then(() => snap.size);
-
-  })
-  .then((numDeleted) => {
-    if (numDeleted === 0) {
-      resolve();
-      return;
-    }
-
-    // Recurse on the next process tick, to avoid exploding the stack.
-    process.nextTick(() => {
-      deleteQueryBatch(query, batchSize, resolve, reject);
-    });
-  })
-  .catch(reject);
-};
-
-// Max docs for batch write is 500
-const deleteCollection = (collectionRef, batchSize = 200) => new Promise((resolve, reject) => {
-  deleteQueryBatch(collectionRef.orderBy('__name__').limit(batchSize), batchSize, resolve, reject);
-});
 
 
 exports.listUpdate = functions.firestore
